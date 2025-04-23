82c8884ae77e81fdfbc6c17dff8b3d65a4d92ad1
Roger0222 <20211113@nbsc.edu.ph>
2025-04-23 13:07:31 +0800

    adding .env file and updates on pubspec.yaml and main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:auth_app/auth/auth_gate.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load();  // Load the environment variables
+
   await Supabase.initialize(
-    anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6cmZ1am9vZGJncXVydWJmbmpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNjQxMzcsImV4cCI6MjA2MDk0MDEzN30.-g-smP2V_3gdwFLOlglH9aFDPDJMzUvIoDto5GaSG9M",
-    url: "https://lzrfujoodbgqurubfnjh.supabase.co",
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,  // Access the anon key from the .env file
+    url: dotenv.env['SUPABASE_URL']!,  // Access the URL from the .env file
   );
+  
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,4 +24,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
